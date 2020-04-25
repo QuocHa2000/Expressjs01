5c109c0016c411669357991a30a2de039e826eea
Ha Vuong Quoc <havuongquoc2000@gmail.com>
2020-04-25 20:00:39 +0700

    Validate user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/users.controller');
+var validate = require('../validate/user.validate');
 
 router.get('/', controller.index);
 router.get('/search',controller.search);
 router.get('/create',controller.get);
 router.get('/:id',controller.view);
-router.post('/create',controller.postCreate);
+router.post('/create',validate.postCreate,controller.postCreate);
 module.exports= router;
